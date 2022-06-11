727f49a94510912d7109c29ac14b8b8c11df0a0c
Rishthegreat <76021793+Rishthegreat@users.noreply.github.com>
2022-06-11 16:48:44 -0400

    Changes by mtrivedi

--- modules/DistanceToShore.py
+++ modules/DistanceToShore.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 url = "https://distance-to-coast-by-point1.p.rapidapi.com/distance-to-coast-point"
@@ -5,15 +7,17 @@ url = "https://distance-to-coast-by-point1.p.rapidapi.com/distance-to-coast-poin
 class shore_distance:
     def __init__(latitude, longitude):
         payload = {
-            "lat": 44.501226,
+            "lat": latitude,
             "lines": False,
-            "lon": -88.062187
+            "lon": longitude
         }
         headers = {
             "content-type": "application/json",
-            "X-RapidAPI-Key": "4593d5ade1msh94b625aa3071c91p1df6f0jsnf2190bc028a6",
+            "X-RapidAPI-Key": "c494f7409cmsha8f0c89234852cbp1657d2jsndd760848d916",
             "X-RapidAPI-Host": "distance-to-coast-by-point1.p.rapidapi.com"
         }
 
         response = requests.request("POST", url, json=payload, headers=headers)
         print(response.text)
+        responsedata = response.json()
+        print(responsedata["nearest_coastline"][0]["dist_meters"])
