39933293202fe9290d0adbf457b4fe102b2d0934
LeverActionGT <63264121+LeverActionGT@users.noreply.github.com>
2022-06-11 12:42:05 -0400

    Update location_information.py

--- modules/location_information.py
+++ modules/location_information.py
@@ -4,11 +4,12 @@ import requests
 import pandas as pd
 import config_reader
 
-class LocationInformation:
+class Geocoding:
     def __init__(self, user_location): #user_location is string address
+        #put input into class attribute
         self.user_location = user_location
 
-    def getLocation(self):
+        #positionstack geocoding API call
         positionstack_token = config_reader.configs()['positionstack']
         request = f'http://api.positionstack.com/v1/forward?access_key={positionstack_token}&query={self.user_location}'
         response = requests.get(request)
@@ -16,15 +17,13 @@ class LocationInformation:
             data = response.json()
             self.latitude = data['data'][0]['latitude']
             self.longitude = data['data'][0]['longitude']
-            return [self.latitude, self.longitude]
-        elif response.status_code == 404:
-            return("error")
+        elif response.status_code == 404: 
+            raise Exception('404, PositionStack API not accessible')
 
-    def getSeaElevation(self):
+        #open-elevation elev API call
         request = f'https://api.open-elevation.com/api/v1/lookup?locations={self.latitude},{self.longitude}'
         r = requests.get(request, timeout=20)
         if r.status_code == 200 | r.status_code == 201:
             self.elevation = pd.json_normalize(r.json(), 'results')['elevation'].values[0]
         else:
             self.elevation = None
-        return self.elevation
