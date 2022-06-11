3a1f500a934913d42b7cd4dec6d8160325be7a5d
Rishthegreat <76021793+Rishthegreat@users.noreply.github.com>
2022-06-11 15:04:37 -0400

    changing some stuff

--- modules/TemperatureModel.py
+++ modules/TemperatureModel.py
@@ -1,19 +1,20 @@
 from datetime import datetime
-from meteostat import Point, Daily
+from meteostat import Point, Monthly
 import matplotlib.pyplot as plt
 
-#class TemperatureModel:
- #   def __init__(self, lat, long):
-  #      self.lat = lat
-   #     self.long = long
-    #    request = f''
-lat = 40
-long = 40
-start = datetime(1970, 1, 1)
-end = datetime(2021, 12, 31)
-testLocation = Point(49.2497, -123.1193, 70)
-data = Daily(testLocation, start, end)
-data = data.fetch()
-data.plot(y=['tavg', 'tmin', 'tmax'])
-plt.show()
+class TemperatureModel:
+    def __init__(self, lat, long, ele):
+        self.lat = lat
+        self.long = long
+        self.ele = ele
+        request = f''
+        start = datetime(1970, 1, 1)
+        end = datetime(2021, 12, 31)
+        testLocation = Point(lat, long, ele)
+        data = Monthly(testLocation, start, end)
+        data = data.fetch()
+        data.to_csv('Australia.csv')
+        data.plot(y=['tavg', 'tmin', 'tmax'])
+        plt.show()
 
+tm = TemperatureModel(-33.854667, 151.225778, 3.3)
